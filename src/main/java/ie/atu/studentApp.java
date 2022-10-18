e8ecc361ddb851ac989529c14c568540e331ff10
Patrick <g00409592@atu.ie>
2022-10-18 13:43:42 +0100

    Creating studentInputTemplate

--- src/main/java/ie/atu/studentApp.java
+++ src/main/java/ie/atu/studentApp.java
@@ -1,4 +1,13 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class studentApp {
+    public static void main(String[] args) {
+        System.out.println("Please enter the name of the first student: ");
+        System.out.println("Please enter the email of the first student: ");
+        System.out.println("Please enter the course of the first student: ");
+        Scanner input = new Scanner(System.in);
+        
+    }
 }
