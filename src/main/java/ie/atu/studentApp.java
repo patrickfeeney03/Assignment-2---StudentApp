bd6008160dce8ca91fd75bacb717c1011af2629c
Patrick <g00409592@atu.ie>
2022-10-18 14:02:40 +0100

    addded temp

--- src/main/java/ie/atu/studentApp.java
+++ src/main/java/ie/atu/studentApp.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class studentApp {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        studentInputTemplate student1 = new studentInputTemplate();
         System.out.println("Please enter the name of the first student: ");
+
+        student1.setName(input.nextLine());
+        System.out.println(student1.getName());
+
+
+
         System.out.println("Please enter the email of the first student: ");
         System.out.println("Please enter the course of the first student: ");
-        Scanner input = new Scanner(System.in);
+
 
     }
 }
